611773e0aac7d6f0ba1560d3cf8f8f049a6a216a
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-09-06 15:06:43 -0300

    Add solution repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'
+    fullString = ''
+    for(let i = 0; i < num; i++) {
+        fullString += string;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
