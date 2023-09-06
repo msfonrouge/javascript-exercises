2fbfd478c59ce93f3f11dad7f6db9982ef5cd7c5
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-09-06 15:22:49 -0300

    Add solution reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray = string.split('');
+    stringArray = stringArray.reverse();
+    string = stringArray.join('');
+    return string;
 };
 
 // Do not edit below this line
