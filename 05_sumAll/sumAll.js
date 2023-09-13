961d453540347b4244010f476ad64f540338e5ae
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-09-13 00:45:03 -0300

    Add solution sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let totalSum = 0;
+    if (
+      num1 < 0 ||
+      typeof num1 !== "number" ||
+      num2 < 0 ||
+      typeof num2 !== "number"
+    ) {
+      return "ERROR";
+    }
+      if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+      }
+    for(let i = num1; i <= num2; i++) {
+        totalSum+=i;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
