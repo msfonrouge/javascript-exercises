83a4288b343a6b90861d6c3e2950802a78f26452
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-09-13 00:14:18 -0300

    Add solution remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...extra) {
+    return array.filter((element) => !extra.includes(element));
 };
 
 // Do not edit below this line
