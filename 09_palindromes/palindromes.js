363e3e58612b4e8dd56c19ec1e62f335ebd69df4
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-09-28 12:51:59 -0300

    Add palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let punctuation = /[\.,?!" "]/g;
+  text = text.toLowerCase().replace(punctuation, "");
+  return text === text.split("").reverse().join("");
 };
 
 // Do not edit below this line
