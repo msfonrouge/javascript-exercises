2ddd28a64fa3d2dc0c2df282b055d858351d715c
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-12-15 03:16:00 -0300

    Add fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
 
+    let a = 0
+    let b = 1
+    let temp
+
+    for (let i = 2; i <= num; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+
+    }
+    return b;
 };
 
 // Do not edit below this line
