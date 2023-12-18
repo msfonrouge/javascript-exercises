ebbdfc94f91c7cfc772a5a44cceb7bd13fc588b6
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-12-18 03:49:23 -0300

    Add solution get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let book_titles = [];
+    books.forEach(book => {
+        book_titles.push(book.title)
+    });
+    return book_titles;
 };
 
 // Do not edit below this line
